831ae98ad3f6231d8d4a617d1bbc905f9beed762
Mule129 <a24cc410s@naver.com>
2022-05-24 00:19:32 +0900

    update_0524

--- PJ test file/testData.py
+++ PJ test file/testData.py
@@ -5,7 +5,7 @@ from mpl_toolkits.mplot3d.axes3d import *
 x, y = np.meshgrid(np.arange(-1,1,0.1), 
 np.arange(-1,1,0.1))
 
-z = x**2 + y**2
+z = x**2 + y**2#피타고라스 
 
 fig = figure()
 ax = fig.add_subplot(111, projection = "3d")
--- PJ test file/testPython.py
+++ PJ test file/testPython.py
@@ -1,8 +1,8 @@
-"""import sys
+import sys
 
 n = sys.stdin.readline()
 
 count = 0
-data = [sys.stdin.readline() if sys.stdin.readline == "ENTER" else "1" for _ in range(int(n)) ]
-print(data)
-"""
+for i in range(n):
+    sys.stdin.readline()
+    
