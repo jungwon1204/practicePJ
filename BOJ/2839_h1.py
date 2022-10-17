d9d7be27d9720f5167eb0dda96da017cfc8d8ab6
Mule129 <a24cc410s@naver.com>
2022-10-18 04:02:04 +0900

    update

--- BOJ/2839_h1.py
+++ BOJ/2839_h1.py
@@ -1,5 +1,15 @@
-N = int(input())
+n = int(input())
+data = 0
+for i in range(n):
+    data = n - (3 * i)
+    if data%5 == 0:
+        print(int(i + data/5))
+        break
+    elif data > n or data < 0:
+        print(-1)
+        break
 
-if N < 8:
-    for i in range(N):
-        print(f"{int(N/5*i)}")
+"""
+n=int(input())
+print(-(n in[4,7])or n-2*n//5*2) < wow
+"""
