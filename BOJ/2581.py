e2ce7d68c8e72e910d41d08f9886b2e0b187e69d
Mule129 <a24cc410s@naver.com>
2022-10-27 01:26:43 +0900

    Update 2581.py

--- BOJ/2581.py
+++ BOJ/2581.py
@@ -1,13 +1,15 @@
 n = int(input()); m = int(input())
-cnt = []
-test = 0
+if n == 1:
+    n = 2
+data = []
+vital = 0
 for i in range(n, m+1):
-    print(i)
     for x in range(2, i):
         if i%x == 0:
-            test = 1
+            vital = 1
             break
-    if test == 0:
-        cnt.append(i)
-    test = 0
-a = print(-1) if cnt == [] else print(f"{sum(cnt)}\n{cnt[0]}")
+    if vital == 0:
+        data.append(i)
+    vital = 0
+
+a = print(-1) if data == [] else print(f"{sum(data)}\n{data[0]}")
