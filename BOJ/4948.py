bd9a29074c4e7b2161a7e94de75c3cea1c3e650d
Mule129 <a24cc410s@naver.com>
2022-11-09 00:33:42 +0900

    Create 4948.py

---
+++ BOJ/4948.py
@@ -0,0 +1,23 @@
+import time
+n = 1
+while True:
+    
+    n = int(input())
+    t1 = time.time()
+    if n == 0:
+        break
+    data = ((n*2)+1)*[True]#0~n*2까지
+    
+    for i in range(len(data)):
+        
+        if i < 2:
+            data[i] = False
+            continue
+        
+        if data[i] == True:
+            for x in range(i+i, len(data), i):
+                data[x] = False
+                
+    t = [i for i, x in enumerate(data) if x == True and i>n]
+    print(len(t))
+    print(time.time() - t1)
